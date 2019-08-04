e82eb02a5d27a9d658d5dff8d59d6acee600b87d
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-08-04 14:32:46 -0600

    socket added

--- source/TX.h
+++ source/TX.h
@@ -1,17 +1,29 @@
 #include <fstream>
+#include <unistd.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <cstdlib>
+#include <utility>
+#include <netinet/in.h>
+using namespace std;
 
 #include "mystruct.h"
 
 class TX {
 public:
-    TX(CMDARGS cmd){
-        info = cmd;
+    explicit TX(CMDARGS cmd){
+        // copy commandline arguments
+        info = std::move(cmd);
+
+        //init socket
+        txSocket = socket(AF_INET, SOCK_STREAM, 0);
         main();
     }
     ~TX()= default;
 
 private:
     CMDARGS info;
+    int txSocket;
 
 
     int main();
