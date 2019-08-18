862e227297b325b9a2fe8ebeffa0aacbff3509c3
pineapple0111 <yuqian.w07@gmail.com>
2019-08-17 22:03:50 -0600

    misc.cpp and misc.h separated to avoid function redefinition

--- source/misc.h
+++ source/misc.h
@@ -14,21 +14,6 @@ struct CMDARGS {
 };
 
 // get the sha265 sum of any file
-std::string shasum(std::string fileName){
-    char buffer[256];
-    std::string result;
-    std::string cmd = "shasum -a 256 ";
-    cmd += fileName;
-    FILE *pipe = popen(cmd.c_str(), "r");
-
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-
-    fgets(buffer, sizeof(buffer), pipe);
-    result += buffer;
-    pclose(pipe);
-    return result;
-}
+std::string shasum(std::string fileName);
 
 
