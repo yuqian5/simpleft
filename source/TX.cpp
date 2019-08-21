2a45174537563a42d4705b75f86cc2aff1743576
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2019-08-21 14:04:04 -0600

    connect timeout updated

--- source/TX.cpp
+++ source/TX.cpp
@@ -4,26 +4,26 @@
 //TODO : add encryption, enable secure transfer (RSA), prevent MITM attack
 
 void TX::socketSetup() {
-    //struct setup
-    address.sin_family = AF_INET;
-    address.sin_port = htons(info.port);
-    if (inet_pton(AF_INET, info.ip.c_str(), &(address.sin_addr)) != 1) {
-        std::cerr << "inet_pton failed" << std::endl;
-        exit(1);
-    }
-
-    //init socket
-    txSocket = socket(PF_INET, SOCK_STREAM, 0);
-    if (txSocket < 0) {
-        std::cerr << "socket() failed" << std::endl;
-        exit(1);
-    }
-
     //connect, timeout after 600s (10min)
     int tries_left = 600;
     cout << "Connecting...";
     flush(cout);
     while(tries_left){
+        //struct setup
+        address.sin_family = AF_INET;
+        address.sin_port = htons(info.port);
+        if (inet_pton(AF_INET, info.ip.c_str(), &(address.sin_addr)) != 1) {
+            std::cerr << "inet_pton failed" << std::endl;
+            exit(1);
+        }
+
+        //init socket
+        txSocket = socket(PF_INET, SOCK_STREAM, 0);
+        if (txSocket < 0) {
+            std::cerr << "socket() failed" << std::endl;
+            exit(1);
+        }
+
         if (connect(txSocket, (struct sockaddr *) &address, sizeof(address)) < 0) {
             --tries_left;
             if(!tries_left){
@@ -43,7 +43,7 @@ void TX::transmit() {
     FILE *fdin;
     cout << "File Name: " << filePath << endl;
 
-    struct stat fileInfo;
+    struct stat fileInfo = {};
     if (stat(filePath.c_str(), &fileInfo) == 0) {
         cout << "File Size: " << fileInfo.st_size << endl;
         //open file
@@ -69,7 +69,7 @@ void TX::transmit() {
         string length_str = to_string(length);
         int diff = 3 - length_str.length();
         for (int i = 0; i < diff; i++) {
-            length_str = "0" + length_str;
+            length_str.insert(0, "0");
         }
         send(txSocket, length_str.c_str(), length_str.length(), 0);
     }
@@ -88,7 +88,7 @@ void TX::transmit() {
     string size_str = to_string(fileInfo.st_size);
     int diff = 13 - (int) size_str.length();
     for (int i = 0; i < diff; i++) {
-        size_str = "0" + size_str;
+        size_str.insert(0, "0");
     }
     send(txSocket, size_str.c_str(), size_str.length(), 0);
 
@@ -113,8 +113,7 @@ void TX::transmit() {
 }
 
 string TX::extractFilename() {
-    unsigned short index = filePath.length();
-    for ( ; index > 0; --index) {
+    for (unsigned short index = filePath.length() ; index > 0; --index) {
         if(filePath[index] == '/'){
             ++index;
             return filePath.substr(index, filePath.length());
