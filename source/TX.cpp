b63c6810fe50ac639b1f9636f558dffda1633d13
yuqian5 <28016308+yuqian5@users.noreply.github.com>
2020-03-29 20:11:46 -0600

    ipv6 support added

--- source/TX.cpp
+++ source/TX.cpp
@@ -1,35 +1,32 @@
 #include "TX.h"
 
-//TODO : serialize data with boost library before sending, enable transfers between machines with different endian.
-//TODO : add encryption, enable secure transfer (RSA), prevent MITM attack
+TX::TX(CMDARGS cmd) {
+    // copy commandline arguments
+    info = std::move(cmd);
+    filePath = info.filePath;
 
-void TX::socketSetup() {
+    socketSetup();
+    transmit();
+}
+
+TX::~TX() {
+    shutdown(this->connectfd, O_RDWR);
+    close(this->connectfd);
+}
+
+void TX::connectNow(sockaddr_in &serverAddr4, int &connectfd) {
     //connect, timeout after 600s (10min)
-    int tries_left = 600;
-    cout << "Connecting...";
-    flush(cout);
-    while(tries_left){
+    int tries_left = 60;
+    std::cout << "Connecting...";
+    while (tries_left) {
         //struct setup
-        address.sin_family = AF_INET;
-        address.sin_port = htons(info.port);
-        if (inet_pton(AF_INET, info.ip.c_str(), &(address.sin_addr)) != 1) {
-            std::cerr << "inet_pton failed" << std::endl;
-            exit(1);
-        }
-
-        //init socket
-        txSocket = socket(PF_INET, SOCK_STREAM, 0);
-        if (txSocket < 0) {
-            std::cerr << "socket() failed" << std::endl;
-            exit(1);
-        }
-
-        if (connect(txSocket, (struct sockaddr *) &address, sizeof(address)) < 0) {
-            --tries_left;
-            if(!tries_left){
-                std::cerr << "\nconnection failed, no receiver found" << std::endl;
+        if (connect(connectfd, (struct sockaddr *) &serverAddr4, sizeof(serverAddr4)) < 0) {
+            std::cerr << '.';
+            tries_left -= 1;
+            if (!tries_left) {
+                std::cerr << "\nConnection Failed, No Receiver Found" << std::endl;
                 exit(1);
-            }else{
+            } else {
                 sleep(1);
             }
         } else {
@@ -39,82 +36,145 @@ void TX::socketSetup() {
     }
 }
 
+void TX::connectNow(sockaddr_in6 &serverAddr6, int &connectfd) {
+    //connect, timeout after 600s (10min)
+    int tries_left = 60;
+    std::cout << "Connecting...";
+    while (tries_left) {
+        //struct setup
+        if (connect(connectfd, (struct sockaddr *) &serverAddr6, sizeof(serverAddr6)) < 0) {
+            std::cout << '.';
+            tries_left -= 1;
+            if (!tries_left) {
+                std::cerr << "\nconnection failed, no receiver found" << std::endl;
+                exit(1);
+            } else {
+                sleep(1);
+            }
+        } else {
+            std::cout << "Connected" << std::endl;
+            break;
+        }
+    }
+}
+
+void TX::socketSetup() {
+    // check protocol standard
+    if (info.standard == "ipv4") { // ipv4
+        // initialize socket
+        this->connectfd = socket(AF_INET, SOCK_STREAM, 0);
+        memset(&this->serverAddr4, '0', sizeof(this->serverAddr4));
+
+        this->serverAddr4.sin_family = AF_INET;
+        this->serverAddr4.sin_port = htons(info.port);
+        if (inet_pton(AF_INET, this->info.ip.c_str(), &(this->serverAddr4.sin_addr)) != 1) {
+            std::cerr << "ERROR: inet_pton failed" << std::endl;
+            exit(1);
+        }
+        // connect
+        connectNow(std::ref(this->serverAddr4), std::ref(this->connectfd));
+    } else { // ipv6
+        // initialize socket
+        this->connectfd = socket(AF_INET6, SOCK_STREAM, 0);
+        memset(&this->serverAddr6, '0', sizeof(this->serverAddr6));
+
+        this->serverAddr6.sin6_family = AF_INET6;
+        this->serverAddr6.sin6_port = htons(this->info.port);
+        if (inet_pton(AF_INET6, this->info.ip.c_str(), &(this->serverAddr6.sin6_addr)) != 1) {
+            std::cerr << "ERROR: inet_pton failed" << std::endl;
+            exit(1);
+        }
+        // connect
+        connectNow(std::ref(this->serverAddr6), std::ref(this->connectfd));
+    }
+}
+
 void TX::transmit() {
+    if (!packFile(info.filePath)) {
+        std::cerr << "ERROR: No such file or directory" << std::endl;
+        exit(0);
+    }
+
+    char readBuf[16];
+    memset(readBuf, 0, sizeof(readBuf));
+
+    // get file info
     FILE *fdin;
-    cout << "File Name: " << filePath << endl;
 
     struct stat fileInfo = {};
-    if (stat(filePath.c_str(), &fileInfo) == 0) {
-        cout << "File Size: " << fileInfo.st_size << endl;
+    if (stat("tempFilePackage.tar.gz", &fileInfo) == 0) {
         //open file
-        fdin = fopen(filePath.c_str(), "rb");
+        fdin = fopen("tempFilePackage.tar.gz", "rb");
         if (fdin == nullptr) {
-            std::cerr << "an error occurred while opening file " << errno << std::endl;
+            std::cerr << "ERROR: Cannot Get File Info" << std::endl;
             exit(1);
         }
     } else {
-        std::cerr << "cannot get file info" << std::endl;
+        std::cerr << "ERROR: Cannot Get File Info" << std::endl;
         exit(1);
     }
 
-    //send filename length
-    string fileName; // just the file name, path removed
+    // get filename
+    std::string fileName; // just the file name, path removed
     fileName = extractFilename();
 
-    int length = fileName.length();
-    if (length > 999) {
-        std::cerr << "filepath too long" << std::endl;
-        exit(1);
-    } else {
-        string length_str = to_string(length);
-        int diff = 3 - length_str.length();
-        for (int i = 0; i < diff; i++) {
-            length_str.insert(0, "0");
-        }
-        send(txSocket, length_str.c_str(), length_str.length(), 0);
-    }
+    // send filename
+    send(this->connectfd, fileName.c_str(), fileName.length(), 0);
 
-    //send filename
-    send(txSocket, fileName.c_str(), fileName.length(), 0);
+    // wait for confirmation
+    recv(this->connectfd, readBuf, sizeof(readBuf), 0);
+    memset(readBuf, 0, sizeof(readBuf));
 
-    //send sha256 sum
-    string result, result_temp;
-    result_temp = shasum(filePath);
-    result = result_temp.substr(0, result_temp.find(' '));
-    std::cout << "SHA256 SUM: " << result << std::endl;
-    send(txSocket, result.c_str(), result.length(), 0);
+    // send sha256 sum
+    std::string sum;
+    sum = shasum();
+    send(this->connectfd, sum.c_str(), sum.length(), 0);
 
-    //send fileSize
-    string size_str = to_string(fileInfo.st_size);
+    // wait for confirmation
+    recv(this->connectfd, readBuf, sizeof(readBuf), 0);
+    memset(readBuf, 0, sizeof(readBuf));
+
+    // send fileSize
+    std::string size_str = std::to_string(fileInfo.st_size);
     int diff = 13 - (int) size_str.length();
     for (int i = 0; i < diff; i++) {
         size_str.insert(0, "0");
     }
-    send(txSocket, size_str.c_str(), size_str.length(), 0);
+    send(this->connectfd, size_str.c_str(), size_str.length(), 0);
 
-    //send file
-    unsigned char toSend[2048];
+    // wait for confirmation
+    recv(this->connectfd, readBuf, sizeof(readBuf), 0);
+    memset(readBuf, 0, sizeof(readBuf));
+
+    // send file
+    unsigned char outBoundData[2048];
     int chunkSize = 2048;
     int fileSize = fileInfo.st_size;
 
-    while(fileSize != 0){
+    while (fileSize != 0) {
         if (chunkSize > fileSize) {
             chunkSize = fileSize;
             fileSize = 0;
-        }else{
-            fileSize-=chunkSize;
+        } else {
+            fileSize -= chunkSize;
         }
-        fread(toSend, chunkSize, 1, fdin);
+        fread(outBoundData, chunkSize, 1, fdin);
 
-        send(txSocket, toSend, chunkSize, 0);
-        memset(toSend, 0, sizeof(toSend));
+        send(this->connectfd, outBoundData, chunkSize, 0);
+        memset(outBoundData, 0, sizeof(outBoundData));
     }
     fclose(fdin);
+
+    // remove file
+    deleteFile();
+
+    // prompt complete
+    std::cout << fileName << " Sent To " << info.ip << std::endl;
 }
 
-string TX::extractFilename() {
-    for (unsigned short index = filePath.length() ; index > 0; --index) {
-        if(filePath[index] == '/'){
+std::string TX::extractFilename() {
+    for (unsigned short index = filePath.length(); index > 0; --index) {
+        if (filePath[index] == '/') {
             ++index;
             return filePath.substr(index, filePath.length());
         }
--- source/TX.h
+++ source/TX.h
@@ -1,41 +1,38 @@
-#include <cstdio>
 #include <unistd.h>
 #include <iostream>
 #include <sys/socket.h>
-#include <cstdlib>
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <cstring>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-using namespace std;
-
 #include "misc.h"
+#include "Transceiver.hpp"
 
-class TX {
+#ifndef LANFT_TX_HPP
+#define LANFT_TX_HPP
+
+class TX : protected Transceiver {
 public:
-    explicit TX(CMDARGS cmd) {
-        // copy commandline arguments
-        info = std::move(cmd);
-        filePath = info.filePath;
+    explicit TX(CMDARGS cmd);
 
-        socketSetup();
-        transmit();
-    }
-
-    ~TX() {
-        shutdown(txSocket, 0);
-    }
+    ~TX();
 
 private:
     CMDARGS info;
-    int txSocket;
-    struct sockaddr_in address;
-    string filePath;
+
+    int connectfd;
+    struct sockaddr_in6 serverAddr6;
+    struct sockaddr_in serverAddr4;
+    std::string filePath;
 
     void socketSetup(); // setup and connect to RX side
     void transmit(); // sending the file
-    string extractFilename(); // extract file name from file path if needed
+    static void connectNow(sockaddr_in6 &serverAddr6, int &connectfd);
+
+    static void connectNow(sockaddr_in &serverAddr4, int &connectfd);
+
+    std::string extractFilename(); // extract file name from file path if needed
 };
+
+#endif //LANFT_TX_HPP
 
